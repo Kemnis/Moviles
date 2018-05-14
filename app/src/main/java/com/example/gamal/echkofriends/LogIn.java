5cd151470d51184591b68c53d23be1b5d7b56480
Unknown <verdeserpiente717@gmail.com>
2018-05-14 00:58:02 -0500

    Ya se puede avanzar entre 2 activities
    Solo se habilito la activity LogIn como principal, ahora puede accederse a la principal desde el login, falta darle buen diseño a LogIn, se espera tenerlo en 1 dia para asi tener tiempo para aplicar el webservice correctamente

--- app/src/main/java/com/example/gamal/echkofriends/LogIn.java
+++ app/src/main/java/com/example/gamal/echkofriends/LogIn.java
@@ -1,13 +1,90 @@
 package com.example.gamal.echkofriends;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+import android.widget.VideoView;
 
 public class LogIn extends AppCompatActivity {
+    Button entrar;
+    TextView Username;
+    TextView Password;
+    CheckBox Session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        entrar = (Button) findViewById(R.id.btnLogIn);
+        Username = (TextView) findViewById(R.id.txtUsername);
+        Password = (TextView) findViewById(R.id.txtPassword);
+        Session = (CheckBox) findViewById(R.id.chkSession);
+
+        entrar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+    }
+    public void login()
+    {
+        final String Usuario = Username.getText().toString();
+        Intent Login = new Intent(LogIn.this, MainActivity.class);
+
+        Login.putExtra("Nombre",Usuario);
+        if(Usuario.isEmpty())
+        {
+            Toast.makeText(LogIn.this,"Faltaron Campos", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Logea nuevoLogeo = new Logea();
+            nuevoLogeo.execute(Usuario);
+            startActivity(Login);
+        }
+    }
+    class Logea extends AsyncTask<String,String,String>
+    {
+        ProgressDialog progressLogin;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressLogin = new ProgressDialog(LogIn.this);
+            progressLogin.setTitle("Cargando.");
+            progressLogin.setMessage("Porfavor espere.");
+            progressLogin.setCancelable(false);
+            progressLogin.show();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            progressLogin.dismiss();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String nombre="";
+            while (nombre =="")
+            {
+                nombre = params[0];
+            }
+            return null;
+        }
+
     }
 }
